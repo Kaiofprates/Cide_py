dcca67b2d7be2755c08bfed3a6047e0859eb3bcf
Kaiofprates <kaioprates@outlook.com>
2018-04-18 00:58:21 -0300

    first commit

---
+++ Cide.py
@@ -0,0 +1,80 @@
+__Author__ = "Kaio Fábio Prates"
+
+import os
+from translate_C import translate 
+from tkinter import *
+from tkinter import filedialog
+
+tela = Tk()
+tela.title("PY_teste")
+tela['bg'] = '#4B0082'
+tela.geometry('600x650')
+
+
+
+
+def escolha():
+
+	tela.filename =  filedialog.askopenfilename(title = "Select file",filetypes = (("codigo em c","*.c"),("Text Files","*.txt"),("all files","*.*")))
+	file = open(tela.filename, "r")
+	listFiles = file.readlines()
+	for i in listFiles[:]:
+		texto.insert(INSERT,i)
+	
+def salvar():
+	a = open("arquivo.c","w")
+	y = texto.get(1.0, END)
+	a.write(y)
+	a.close
+
+def copilar():
+	os.system("gnome-terminal -- gcc -Wall arquivo.c -o arquivo")
+	os.system("gnome-terminal -- ./arquivo")
+	
+	
+
+def pseudocodigo():
+	y = texto.get(1.0, END)
+	y = y.replace("escreva","printf")
+	y = y.replace("receba","scanf")
+	y = y.replace("mostre","printf")
+	y = y.replace("leia","scanf")
+	texto.delete(1.0,END)
+	x = "#include <stdio.h>\n#include <stdlib.h>\n\nint main(){\n\n"
+	texto.insert(INSERT,x)
+	texto.insert(INSERT,y)
+	texto.insert(INSERT,"\n\ngetchar();\n\nreturn 0;\n}")
+
+#struct = lambda: texto.insert(CURRENT,"struct funcao { };")
+def traduz():
+	x = translate(texto.get(1.0,END))
+	texto.delete(1.0,END)
+	texto.insert(INSERT,x)
+
+
+texto = Text(tela,height = 40, width = 70,bg = "white", fg ="black")
+texto.tag_config("a", foreground="blue", underline=1)
+#button = Button(texto, text="Click", command=salvar)
+#texto.window_create(INSERT, window=button)
+texto.config(cursor="arrow")
+texto.place(x = 55, y =60)
+
+bot1 = Button(tela,text = "salvar",bg = '#4B0082', fg = 'white', command = salvar)
+bot1.place(x = 150, y = 20)
+
+bot2 = Button(tela,text = "copilar",bg = '#4B0082', fg = 'white', command = copilar)
+bot2.place(x = 250, y = 20)
+
+bot3 = Button(tela,text = "Pseu",height = 2, width = 1, bg = '#4B0082', fg = 'black', command = pseudocodigo)
+bot3.place(x = 5, y = 60)
+
+bot4 = Button(tela,text = "Repl",height = 2, width = 1, bg = '#4B0082', fg = 'black', command = traduz)
+bot4.place(x = 5, y = 100)
+
+bot = Button(tela,text = "Pesquisar",bg = '#4B0082', fg = 'white', command = escolha)
+bot.place(x = 30, y = 20)
+
+
+
+
+tela.mainloop()
---
+++ translate_C.py
@@ -0,0 +1,89 @@
+#Author = Kaio Prates
+#encoding = utf-8
+
+import re
+
+def translate(res):
+	#arq = open("translate.txt","w")
+	#code = open(texto,"r")
+	#res = code.read()
+	var = re.findall(r"int(.*);",res)
+	inteiro = []
+	for i in var[:]:
+		inteiro.append(i)
+	var = re.findall(r"float(.*);",res)
+	real = []
+	for i in var[:]:
+		real.append(i)
+	var = re.findall(r"char(.*);",res)
+	char = []
+	for i in var[:]:
+		char.append(i)
+	var = re.findall(r"leia(.*)?;",res)
+
+	var  = str(var);var = var.replace(")","");var = var.replace("[","");var = var.replace("'","")
+	var = var.replace("(","");var = var.replace("]","");var = var.replace(" ","")
+	var = var.split(",")
+
+
+	f = str(real)
+	f= f.replace(")","");f = f.replace("[","");f = f.replace("'","")
+	f = f.replace("(","");f = f.replace("]","");f = f.replace(" ","")
+	f = f.split(",")
+
+
+	if type(var) == list: 
+		for i in var[:]:
+			for j in f[:]:
+				if i == j:
+				
+					s = "leia("+i+");"
+					d = "scanf("+""" "%f" """+", &"+i+");"
+
+					res = res.replace(s,d)
+
+	if type(var) == str:
+		for i in f[:]:
+			if i == var:
+		
+				s = "leia("+i+");"
+				d = "scanf("+""" "%f" """+", &"+i+");"
+				res = res.replace(s,d)
+
+
+	inteiro = str(inteiro)
+	inteiro= inteiro.replace(")","");inteiro = inteiro.replace("[","");inteiro = inteiro.replace("'","")
+	inteiro = inteiro.replace("(","");inteiro = inteiro.replace("]","");inteiro = inteiro.replace(" ","")
+	inteiro = inteiro.split(",")
+
+
+	if type(var) == list: 
+		for i in var[:]:
+			for j in inteiro[:]:
+				if i == j:
+				
+					s = "leia("+i+");"
+					d = "scanf("+""" "%i" """+", &"+i+");"
+
+					res = res.replace(s,d)
+
+	if type(var) == str:
+		if i in inteiro[:]:
+			if i == var:
+		
+				s = "leia("+i+");"
+				d = "scanf("+""" "%i" """+", &"+i+");"
+				res = res.replace(s,d)
+	res = res.replace("escreva","printf")
+
+	header = "#include <stdio.h>\n#include <stdlib.h>\nint main(){\n"
+	end = "\ngetchar();\nreturn 0;\n}"
+	codfim = (header+res+end)
+	#arq.write(codfim)
+	#arq.close()
+
+	return codfim
+
+
+#a = translate(input("codigo: "))
+#print(a)
